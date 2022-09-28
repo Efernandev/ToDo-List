433f3fd490b7e80347b22d0b545bcdbcd9abcd26
Efernandev <contato.dudu087@gmail.com>
2022-09-27 22:26:18 -0300

    global.css import removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './global.css'
 import './App.module.css'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard/Dashboard'
+import { GlobalStyle } from './styles/GlobalStyle'
 function App() {
-
 	return (
 		<div className="App">
+			<GlobalStyle />
 			<Header />
 			<Dashboard />
 		</div>
