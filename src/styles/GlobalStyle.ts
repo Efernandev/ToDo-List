0c02d10010a8a197bcb1e37e73208ceb5eeba737
Efernandev <contato.dudu087@gmail.com>
2022-09-27 22:25:02 -0300

    global styles added

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,26 @@
-import { createGlobalStyle } from "styled-components";
-
+import { createGlobalStyle, css } from "styled-components";
+const Inter = css`
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700;800;900&display=swap');
+`
 export const GlobalStyle = createGlobalStyle`
+	${Inter}
+	:root {
+		--white:#ffffff;
+		/* grays palletes */
+		--gray-300: #808080;
+		--gray-400: #333333;
+		--gray-500: #262626;
+		--gray-600: #1A1A1A;
+		--gray-700: #0D0D0D;
+
+		/* primary colors */
+		--purple:#8284FA;
+		--purple-dark:#5E60CE;
+		--blue:#4EA8DE;
+		--blue-dark:#1E6F9F;
+		--danger:#E25858;
+	}
+
 	*{
 		padding: 0;
 		margin: 0;
